385ef073f5583160f83bccff020229fe46df6ccc
Tahir Tariq <shehbaztariq1988@gmail.com>
2021-05-18 11:28:26 +0500

    Update album.js

--- album.js
+++ album.js
@@ -1,19 +1,51 @@
-$(document).ready(function () {
-    show_albums(); 
+$(function() {
+    loadAlbums();
+    $("#albums").on("click", ".btn-danger", handleDelete);
+
 });
-function show_albums (){
+
+function handleDelete() {
+    var btn = $(this);
+    var parentDiv = btn.closest(".albums");
+    let id = parentDiv.attr("data-id");
+    console.log(id);
     $.ajax({
-        meyhod : 'GET',
-        url: "https://jsonplaceholder.typicode.com/albums",
-        success: function (response) {
-            response.forEach(function(temp) {
-                // console.log(temp.name);
-                $('#t_body').append("<tr ><td>"+temp.name+"</td><td>"+temp.email+"</td><td><button data-id = "+temp.id+" class='btn btn-primary update' >Update</button></td></tr>");
-            });
-            $('.update').click(function (e) {
-                
-                
-            });
-        }
+      url: "https://jsonplaceholder.typicode.com/albums/" + id,
+      method: "DELETE",
+      error: function(response) {
+        var albums = $("#albums");
+        albums.html("An Error has occured");
+      },
+      success: function() {
+        loadAlbums();
+      }
     });
+  }
+
+function loadAlbums() {
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/albums",
+      method: "GET",
+      error: function(response) {
+        var albums = $("#albums");
+        albums.html("An Error has occured");
+      },
+      success: function(response) {
+        console.log(response);
+        var albums = $("#albums");
+        albums.empty();
+        for (var i = 0; i < response.length; i++) {
+          var alb = response[i];
+          albums.append(
+            `<div class="albums container" data-id="${alb.id}">
+            <h3>${alb.title}</h3>
+            <h5>ID: ${alb.id}</h5>
+            <p>
+            <button class="btn btn-warning btn-sm m-1 float-right">Update Title</button>
+            <button class="btn btn-danger btn-sm m-1 float-right">Delete Album</button> 
+            UserID: ${alb.userId}</p></div>`
+          );
+        }
+      }
+    })
 }
