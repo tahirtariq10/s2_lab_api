7757aed6d6be25a030752fd8a17f5a77803c94ad
Tahir Tariq <shehbaztariq1988@gmail.com>
2021-05-18 10:43:49 +0500

    Update album.js

--- album.js
+++ album.js
@@ -1,3 +1,19 @@
-$(function() {
-    
+$(document).ready(function () {
+    show_albums(); 
 });
+function show_albums (){
+    $.ajax({
+        meyhod : 'GET',
+        url: "https://jsonplaceholder.typicode.com/albums",
+        success: function (response) {
+            response.forEach(function(temp) {
+                // console.log(temp.name);
+                $('#t_body').append("<tr ><td>"+temp.name+"</td><td>"+temp.email+"</td><td><button data-id = "+temp.id+" class='btn btn-primary update' >Update</button></td></tr>");
+            });
+            $('.update').click(function (e) {
+                
+                
+            });
+        }
+    });
+}
